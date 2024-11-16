b5ce44ec001db1dfe00115cad1676acdc3d19d8a
Sam Annetts <samannetts8@gmail.com>
2024-11-16 23:17:32 +0000

    Removed constant from js file and added .img_enter_screen

--- recipe_java.js
+++ recipe_java.js
@@ -8,8 +8,6 @@ const enter_button = document.getElementById("enter_button");
 const main_heading = document.getElementById("main_heading");
 const text_intro = document.getElementById("text_intro");
 const welcome_message = document.getElementById("welcome_message");
-const recipe_links = document.getElementsByClassName("recipe_links")
-
 
 enter_button.addEventListener("click", () => {
     enter_button.classList.add("animate_enter_button");
@@ -19,5 +17,4 @@ enter_button.addEventListener("click", () => {
     setTimeout(() => {main_heading.classList.add("enter_screen");},1000);    
     setTimeout(() => {text_intro.classList.add("enter_screen");},1500);        
     setTimeout(() => {welcome_message.classList.add("enter_screen");},2000);        
-    setTimeout(() => {recipe_links.classList.add("enter_screen");},2500);
 });
