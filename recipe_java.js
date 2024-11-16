65c6b6df33806e4d26ce48ee2a4d3caa70378079
Sam Annetts <samannetts8@gmail.com>
2024-11-16 16:15:18 +0000

    Reformatted enter_button and added animation
    Introduced addEventListener to respond to enter_button being pressed.
    
    Added delay to <script> tag in html file to ensure DOM could be used for const enter_button.
    
    Removed onclick="showIntroPage()" from html.
    
    Introduced shrinkAndGrow animation + included keyframes breakdown
    
    Introduced cursor:pointer to css stylesheet.
    
    Centered Enter button on page with new body CSS section.

--- recipe_java.js
+++ recipe_java.js
@@ -2,3 +2,14 @@ function showIntroPage() {
     document.querySelector('.post_button').style.display='block';
     document.querySelector('#enter_button').style.display='none';
 }
+
+//Button Press Animation
+const enter_button = document.getElementById("enter_button");
+
+enter_button.addEventListener("click", () => {
+    enter_button.classList.add("animate_enter_button");
+    setTimeout(() => {enter_button.classList.remove("animate_enter_button");
+        showIntroPage();
+    },400);
+
+});
